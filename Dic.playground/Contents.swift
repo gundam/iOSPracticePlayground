027d116679770bebd6f44e4598407cd86ab5605f
austin.yuan <austin.yuan@neulion.com.cn>
2018-08-01 17:31:20 +0800

    update dic

---
+++ Dic.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+// DIC
+import UIKit
+
+var str = "Hello, playground"
+
+var nums : Dictionary = [String:Any]()
+nums["ket"] = "ss"
+nums
+
+
+
