2c54add07d7ad66f9a0baedac1d092be76104b53
austin.yuan <austin.yuan@neulion.com.cn>
2018-08-01 17:19:31 +0800

    update Stack

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -11,6 +11,36 @@ class ASStack{
     func size() -> Int {
         return stack.count
     }
+    init() {
+        stack = [Any]()
+    }
+    func pop() -> Any? {
+        return stack.removeLast()
+    }
     
+    func push(object : Any) -> ()  {
+        stack.append(object)
+    }
 }
+
+extension ASStack : CustomStringConvertible{
+    var description: String {
+        get {
+            var outString : String = ""
+            for item : Any in self.stack {
+                let temp =  item as! String
+                outString += temp
+            }
+            return outString
+        }
+        
+    }
+}
+
+var stack = ASStack()
+stack.size()
+stack.push(object: "xx  c")
+stack.push(object: "12xxwwww")
+stack.size()
+print(stack)
 
