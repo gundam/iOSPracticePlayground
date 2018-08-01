e620cfe89c2ca0988a6805ae757a9ff894bf3c79
austin.yuan <austin.yuan@neulion.com.cn>
2018-08-01 16:58:31 +0800

    update Stack

---
+++ Stack.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+// about Stack
+// stack: first in first out
+import UIKit
+
+var str = "Hello, playground"
+
+class ASStack{
+    var stack : [Any] = []
+    
+    func size() -> Int {
+        return stack.count
+    }
+    
+}
+
