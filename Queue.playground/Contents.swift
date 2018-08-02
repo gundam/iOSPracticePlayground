450cfc0e72e4fe11371feb8ad0f164f13cb3ccea
austin.yuan <austin.yuan@neulion.com.cn>
2018-08-02 10:41:30 +0800

    update Queue

---
+++ Queue.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+protocol Queue{
+    associatedtype Element
+    var isEmpty: Bool{get}
+    
+    var size: Int{get}
+    
+    var peek:
+}
