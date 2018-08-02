1cbf7d0b716bbd7f6eba93997edee0f0af6bad02
austin.yuan <austin.yuan@neulion.com.cn>
2018-08-02 10:58:30 +0800

    update Queue

--- Queue.playground/Contents.swift
+++ Queue.playground/Contents.swift
@@ -10,5 +10,46 @@ protocol Queue{
     
     var size: Int{get}
     
-    var peek:
+    var peek:Element?{ get }
+    
+    mutating func enqueue(_element:Element)
+    
+    mutating func dequeue() -> Element?
 }
+
+
+struct IntergerQueue : Queue{
+    typealias Element = Int
+    private var queue = [Element]()
+    var isEmpty: Bool{
+        return queue.isEmpty
+    }
+    var size: Int{
+        return queue.count
+    }
+    var peek: Element?{
+        return queue.first
+    }
+    
+    mutating func enqueue(_element: Element) {
+        queue.append(_element)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return queue.removeFirst()
+    }
+}
+
+var testQueue = IntergerQueue()
+testQueue.enqueue(_element: 2)
+testQueue.enqueue(_element: 3)
+testQueue.enqueue(_element: 4)
+testQueue.peek
+testQueue.isEmpty
+testQueue.dequeue()
+testQueue.isEmpty
+testQueue.dequeue()
+testQueue.enqueue(_element: 5)
+testQueue.dequeue()
+testQueue.dequeue()
+
